a3f2b3e73edb60383da91bd7ceac0c182f942955
Donna Cruze <donneunice@hotmaile.com>
2023-02-15 15:27:54 +0000

    glósa
    hvað er ég að gera?

--- Tölvunarfræði02/Glósur/aefingar/src/ResizingArrayQueueDemo.java
+++ Tölvunarfræði02/Glósur/aefingar/src/ResizingArrayQueueDemo.java
@@ -1,12 +1,12 @@
-public class ResizingArrayQueueDemo {
+public class test {
     public static void main(String[] args) {
-        ResizingArrayQueue<Integer> queue = new ResizingArrayQueue<Integer>();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        System.out.println(queue.dequeue());
-        System.out.println(queue.dequeue());
-        System.out.println(queue.size());
-        System.out.println(queue.isEmpty());
+        QueueOfStrings q = new QueueOfStrings;
+                while (!StdIn.isEmpty()) {
+                    String s = StdIn.readString;
+                    if (s.equals("-")) StdOut.print(q.dequeue());
+                    else q.enqueue(s);
+                }
+
     }
 }
+
